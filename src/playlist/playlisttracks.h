b13da71f9cc4bdf502113fca1213185014af2882
pafe <pafe@921edcb0-ef9c-43ba-a9eb-55e1b4e8fcd7>
2013-12-11 10:13:34 +0000

    Speichern von Playlists implementiert

--- src/playlist/playlisttracks.h
+++ src/playlist/playlisttracks.h
@@ -9,7 +9,34 @@
 #define PLAYLISTTRACKS_H_
 
 #include <QTreeWidget>
+#include "winmusik3.h"
+
 class Playlist;
+
+class PlaylistItem : public QTreeWidgetItem
+{
+	public:
+		ppluint32	titleId;
+		int			startPositionSec;
+		int			endPositionSec;
+		int			cutStartPosition[5];
+		int			cutEndPosition[5];
+		ppl6::CString	Artist;
+		ppl6::CString	Title;
+		ppl6::CString	Version;
+		ppl6::CString	Genre;
+		ppl6::CString	Label;
+		ppl6::CString	Album;
+		ppl6::CString	File;
+		int				musicKey;
+		int				bpm;
+		int				rating;
+		int				length;
+
+		PlaylistItem();
+		ppl6::CString exportAsXML(int indention=3) const;
+};
+
 
 class PlaylistTracks : public QTreeWidget
 {
@@ -18,11 +45,22 @@ class PlaylistTracks : public QTreeWidget
 		void mousePressEvent ( QMouseEvent * event );
 		void mouseReleaseEvent ( QMouseEvent * event );
 
+		void saveWMP(const ppl6::CString &Filename);
+
 		Playlist *playlist;
+
+		ppl6::CString Name;
 
 	public:
 		PlaylistTracks(QWidget * parent = 0);
 		void setPlaylist(Playlist *p);
+
+		void unselectItems();
+		void deleteSelectedItems();
+		void deleteItems(QList<QTreeWidgetItem *>items);
+
+		void save(const ppl6::CString &Filename);
+		void setName(const ppl6::CString &Name);
 
 };
 
