4a312ef7971b633d75dd484ecfe7cac3f047f5f6
pafe <pafe@921edcb0-ef9c-43ba-a9eb-55e1b4e8fcd7>
2013-12-27 17:08:43 +0000

    Unterstützung für OpenKey

--- src/StorageTitle.cpp
+++ src/StorageTitle.cpp
@@ -635,36 +635,68 @@ ppl6::CString DataTitle::keyName(int id)
 	}
 }
 
+ppl6::CString DataTitle::openKeyName(int id)
+{
+	switch (id) {
+		case 1: return " 6m";	// G#m
+		case 2: return " 6d";	// B
+		case 3: return " 7m";	// D#m
+		case 4: return " 7d";	// F#
+		case 5: return " 8m";	// A#m
+		case 6: return " 8d";	// C#
+		case 7: return " 9m";	// Fm
+		case 8: return " 9d";	// G#
+		case 9: return "10m";	// Cm
+		case 10: return "10d";	// D#
+		case 11: return "11m";	// Gm
+		case 12: return "11d";	// A#
+		case 13: return "12m";	// Dm
+		case 14: return "12d";	// F
+		case 15: return " 1m";	// Am
+		case 16: return " 1d";	// C
+		case 17: return " 2m";	// Em
+		case 18: return " 2d";	// G
+		case 19: return " 3m";	// Bm
+		case 20: return " 3d";	// D
+		case 21: return " 4m";	// F#m
+		case 22: return " 4d";	// A
+		case 23: return " 5m";	// C#m
+		case 24: return " 5d";	// E
+		default: return "";		// ""
+	}
+}
+
+
 int DataTitle::keyId(const ppl6::CString &name)
 {
 	ppl6::CString k=ppl6::LCase(ppl6::Trim(name));
 	k.Replace("♯","#");
 
 	if (k.IsEmpty()) return 0;
-	if (k=="g#m" || k=="g#min" || k=="abm" || k=="1a") return 1;
-	else if (k=="b" || k=="bmaj" || k=="1b") return 2;
-	else if (k=="d#m" || k=="d#min" || k=="ebm" || k=="2a") return 3;
-	else if (k=="f#" || k=="f#maj" || k=="gb" || k=="2b") return 4;
-	else if (k=="a#m" || k=="a#min" || k=="bbm" || k=="3a") return 5;
-	else if (k=="c#" || k=="c#maj" || k=="db" || k=="3b") return 6;
-	else if (k=="fm" || k=="fmin" || k=="4a") return 7;
-	else if (k=="g#" || k=="g#maj" || k=="ab" || k=="4b") return 8;
-	else if (k=="cm"|| k=="cmin" || k=="5a") return 9;
-	else if (k=="d#" || k=="d#maj" || k=="eb" || k=="5b") return 10;
-	else if (k=="gm" || k=="gmin" || k=="6a") return 11;
-	else if (k=="a#" || k=="a#maj" || k=="bb" || k=="6b") return 12;
-	else if (k=="dm" || k=="dmin" || k=="7a") return 13;
-	else if (k=="f" || k=="fmaj" || k=="7b") return 14;
-	else if (k=="am" || k=="amin" || k=="8a") return 15;
-	else if (k=="c" || k=="cmaj" || k=="8b") return 16;
-	else if (k=="em" || k=="emin" || k=="9a") return 17;
-	else if (k=="g" || k=="gmaj" || k=="2b") return 18;
-	else if (k=="bm" || k=="bmin" || k=="10a") return 19;
-	else if (k=="d" || k=="dmaj" || k=="10b") return 20;
-	else if (k=="f#m" || k=="f#min" || k=="gbm" || k=="11a") return 21;
-	else if (k=="a" || k=="amaj" || k=="11b") return 22;
-	else if (k=="c#m" || k=="c#min" || k=="dbm" || k=="12a") return 23;
-	else if (k=="e" || k=="emaj" || k=="12b") return 24;
+	if (k=="g#m" || k=="g#min" || k=="abm" || k=="1a" || k=="6m") return 1;
+	else if (k=="b" || k=="bmaj" || k=="1b" || k=="6d") return 2;
+	else if (k=="d#m" || k=="d#min" || k=="ebm" || k=="2a" || k=="7m") return 3;
+	else if (k=="f#" || k=="f#maj" || k=="gb" || k=="2b" || k=="7d") return 4;
+	else if (k=="a#m" || k=="a#min" || k=="bbm" || k=="3a" || k=="8m") return 5;
+	else if (k=="c#" || k=="c#maj" || k=="db" || k=="3b" || k=="8d") return 6;
+	else if (k=="fm" || k=="fmin" || k=="4a" || k=="6m" || k=="9m") return 7;
+	else if (k=="g#" || k=="g#maj" || k=="ab" || k=="4b" || k=="9d") return 8;
+	else if (k=="cm"|| k=="cmin" || k=="5a" || k=="10m") return 9;
+	else if (k=="d#" || k=="d#maj" || k=="eb" || k=="5b" || k=="10d") return 10;
+	else if (k=="gm" || k=="gmin" || k=="6a" || k=="11m") return 11;
+	else if (k=="a#" || k=="a#maj" || k=="bb" || k=="6b" || k=="11d") return 12;
+	else if (k=="dm" || k=="dmin" || k=="7a" || k=="12m") return 13;
+	else if (k=="f" || k=="fmaj" || k=="7b" || k=="12d") return 14;
+	else if (k=="am" || k=="amin" || k=="8a" || k=="1m") return 15;
+	else if (k=="c" || k=="cmaj" || k=="8b" || k=="1d") return 16;
+	else if (k=="em" || k=="emin" || k=="9a" || k=="2m") return 17;
+	else if (k=="g" || k=="gmaj" || k=="2b" || k=="2d") return 18;
+	else if (k=="bm" || k=="bmin" || k=="10a" || k=="3m") return 19;
+	else if (k=="d" || k=="dmaj" || k=="10b" || k=="3d") return 20;
+	else if (k=="f#m" || k=="f#min" || k=="gbm" || k=="11a" || k=="4m") return 21;
+	else if (k=="a" || k=="amaj" || k=="11b" || k=="4d") return 22;
+	else if (k=="c#m" || k=="c#min" || k=="dbm" || k=="12a" || k=="5m") return 23;
+	else if (k=="e" || k=="emaj" || k=="12b" || k=="5d") return 24;
 	return 0;
 }
 
