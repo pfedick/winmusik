71718741d23cebedb01c7b36fa908afd5d7346f8
pfedick <patrick@pfp.de>
2016-06-20 11:04:35 +0200

    prepare for new winmusik library

---
+++ lib/include/libwinmusik3.h
@@ -0,0 +1,15 @@
+/*
+ * libwinmusik3.h
+ *
+ *  Created on: 20.06.2016
+ *      Author: patrick
+ */
+
+#ifndef LIBWINMUSIK3_H_
+#define LIBWINMUSIK3_H_
+
+
+
+
+
+#endif /* LIBWINMUSIK3_H_ */
---
+++ lib/src/object_title.cpp
@@ -0,0 +1,5 @@
+#include "libwinmusik3.h"
+
+
+
+
