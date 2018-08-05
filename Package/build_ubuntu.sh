bcc68d707bc807e38f454f240eca8e01b5537ab9
pfedick <patrick@pfp.de>
2018-08-05 11:13:14 +0200

    updated ubuntu build

--- Package/build_ubuntu.sh
+++ Package/build_ubuntu.sh
@@ -1,3 +1,44 @@
+#!/bin/sh
+#
+# This file is part of WinMusik 3 by Patrick Fedick
+#
+# $Author: pafe $
+# $Revision: 404 $
+# $Date: 2014-05-28 19:15:08 +0200 (Mi, 28 Mai 2014) $
+# $Id: build_package.sh 404 2014-05-28 17:15:08Z pafe $
+#
+#
+# Copyright (c) 2014 Patrick Fedick <patrick@pfp.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+CUR=`pwd`
+
+if [ -f "WinMusik.pro" ] ; then
+    CUR=`pwd`
+elif [ -f "build_source.sh" ] ; then
+    cd ..
+    CUR=`pwd`
+else
+    echo "ERROR: Wrong directory, did not found sources"
+    exit 1
+fi
+. $CUR/Package/config.sh
+
+
+
 check_debian_package()
 {
         PKG=$1
@@ -25,7 +66,7 @@ debian_write_control ()
         echo "Depends: $DEPENDS"
         echo "Installed-Size: 1000"
         echo "Description: $DESCRIPTION"
-        cat winmusik/README_en.TXT | while read line
+        cat ../README_en.TXT | while read line
         do
                 if [ -z "$line" ] ; then
                     echo " ."
@@ -49,13 +90,12 @@ ubuntu_check_dependencies ()
             check_debian_package "libssl-dev"
             check_debian_package "libcurl4-openssl-dev"
             check_debian_package "libmcrypt-dev"
-            check_debian_package "libpng12-dev"
+            check_debian_package "libpng-dev"
             check_debian_package "libbz2-dev"
             check_debian_package "zlib1g-dev"
             check_debian_package "libcdio-dev"
-            check_debian_package "libqt4-dev"
-            check_debian_package "qt4-dev-tools"
-            check_debian_package "qt4-qmake"
+            check_debian_package "qt5-default"
+            check_debian_package "qt5-qmake"
             check_debian_package "nasm"
             check_debian_package "dpkg-dev"
         
@@ -71,61 +111,25 @@ ubuntu_check_dependencies ()
     echo "INFO: all required packages are installed"
 }
 
-ubuntu_configure ()
-{
-    if [ ! -f ".configure_done" ] ; then
-	./configure --with-pcre=/usr --with-libiconv-prefix --with-nasm --with-libmcrypt-prefix \
-		--without-libmhash --without-postgresql --with-png --with-jpeg --without-lame \
-		--without-ft-prefix --without-libmad --without-lame --without-x --without-mysql \
-		--with-libcurl --with-sdl-prefix=$CUR --without-libldns --without-freetds \
-		--without-ogg --without-libshout --without-sqlite
-	if [ $? -ne 0 ] ; then
-		exit 1
-	fi
-	touch .configure_done
-    else
-	echo "INFO: configure already done"
-    fi
-}
-
-ubuntu_make ()
-{
-    if [ ! -f ".make_done" ] ; then
-	make
-	if [ $? -ne 0 ] ; then
-		exit 1
-	fi
-	touch .make_done
-    else
-	echo "INFO: make already done"
-    fi
-}
-
-if [ "$DISTRIB_ID" != "Ubuntu" ] ; then
-	echo "ERROR: This is not an ubuntu system"
-	exit 1
-fi
 
 ubuntu_check_dependencies
-ubuntu_configure
-ubuntu_make
 
 echo "INFO: Build Debian-Packet for $DISTRIB_ID $DISTRIB_RELEASE: $DISTRIB_CODENAME"
 DISTNAME="$PROGNAME-$VERSION"
 rm -rf debian
-create_dir debian
-create_dir debian/DEBIAN
-create_dir debian/usr/bin
-create_dir debian/usr/share/applications
-create_dir debian/usr/share/pixmaps
-create_dir debian/usr/share/doc/$PROGNAME
-chmod -R 755 debian
-cp winmusik/release/$PROGNAME debian/usr/bin/$PROGNAME
-cp winmusik/resources/icon64.png debian/usr/share/pixmaps/$PROGNAME.png
-cp winmusik/docs/Userguide_de.pdf debian/usr/share/doc/$PROGNAME
+create_dir tmp/debian
+create_dir tmp/debian/DEBIAN
+create_dir tmp/debian/usr/bin
+create_dir tmp/debian/usr/share/applications
+create_dir tmp/debian/usr/share/pixmaps
+create_dir tmp/debian/usr/share/doc/$PROGNAME
+chmod -R 755 tmp/debian
+cp release/$PROGNAME tmp/debian/usr/bin/$PROGNAME
+cp resources/icon64.png tmp/debian/usr/share/pixmaps/$PROGNAME.png
+cp docs/Userguide_de.pdf tmp/debian/usr/share/doc/$PROGNAME
 
 DEPENDS=""
-cat WinMusik.desktop | sed "s/Exec=.*/Exec=\/usr\/bin\/$PROGNAME/" | sed "s/Icon=.*/Icon=\/usr\/share\/pixmaps\/$PROGNAME.png/" > debian/usr/share/applications/$PROGNAME.desktop
+cat Package/WinMusik.desktop.in | sed "s/Exec=.*/Exec=\/usr\/bin\/$PROGNAME/" | sed "s/Icon=.*/Icon=\/usr\/share\/pixmaps\/$PROGNAME.png/" > tmp/debian/usr/share/applications/$PROGNAME.desktop
 
 (
 	echo "$PROGNAME ($VERSION) unstable; urgency=low"
@@ -138,7 +142,10 @@ cat WinMusik.desktop | sed "s/Exec=.*/Exec=\/usr\/bin\/$PROGNAME/" | sed "s/Icon
 	echo "Local variables:"
 	echo "mode: debian-changelog"
 	echo "End:"
-) > debian/changelog
+) > tmp/debian/changelog
+
+
+cd tmp
 
 debian_write_control
 dpkg-shlibdeps debian/usr/bin/$PROGNAME
@@ -156,17 +163,4 @@ fi
 echo "DONE. Debian package successfully created: $DISTNAME-$DISTRIB_ID-$DISTRIB_RELEASE-$PLATFORM.deb"
 echo ""
 echo "Install with sudo dpkg -i $DISTNAME-$DISTRIB_ID-$DISTRIB_RELEASE-$PLATFORM.deb"
-
-
-
-
-
-
-
-
-
-
-
-
-
 
