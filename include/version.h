c766c1f21c9d55f938c08d3e5f94edc77230ecbe
Patrick Fedick <patrick@pfp.de>
2019-10-13 12:50:37 +0200

    WIP

--- include/version.h
+++ include/version.h
@@ -1,6 +1,6 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 #define WM_VERSION	"3.5.2"
-#define WM_SVN_REVISION	"54"
-#define WM_RELEASEDATE 20181127
+#define WM_SVN_REVISION	"62"
+#define WM_RELEASEDATE 20191012
 #endif /* VERSION_H_ */
