d9de915ef361b4958675af1b50000fe7e9861a73
Patrick Fedick <patrick@pfp.de>
2020-08-01 15:16:43 +0200

    dark color scheme on windows 10

--- gui/src/main.cpp
+++ gui/src/main.cpp
@@ -33,6 +33,8 @@
 
 #include <QtGui>
 #include <QApplication>
+#include <QSettings>
+#include <QStyleFactory>
 #ifdef HAVE_PYTHON
 #include <Python.h>
 #endif
@@ -61,6 +63,37 @@ int main(int argc, char *argv[])
 
     QApplication a(argc, argv);
 
+#ifdef Q_OS_WIN
+    QSettings settings("HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize",QSettings::NativeFormat);
+    if(settings.value("AppsUseLightTheme")==0){
+        qApp->setStyle(QStyleFactory::create("Fusion"));
+        QPalette darkPalette;
+        QColor darkColor = QColor(45,45,45);
+        QColor disabledColor = QColor(127,127,127);
+        darkPalette.setColor(QPalette::Window, darkColor);
+        darkPalette.setColor(QPalette::WindowText, Qt::white);
+        darkPalette.setColor(QPalette::Base, QColor(18,18,18));
+        darkPalette.setColor(QPalette::AlternateBase, darkColor);
+        darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+        darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+        darkPalette.setColor(QPalette::Text, Qt::white);
+        darkPalette.setColor(QPalette::Disabled, QPalette::Text, disabledColor);
+        darkPalette.setColor(QPalette::Button, darkColor);
+        darkPalette.setColor(QPalette::ButtonText, Qt::white);
+        darkPalette.setColor(QPalette::Disabled, QPalette::ButtonText, disabledColor);
+        darkPalette.setColor(QPalette::BrightText, Qt::red);
+        darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+
+        darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+        darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+        darkPalette.setColor(QPalette::Disabled, QPalette::HighlightedText, disabledColor);
+
+        qApp->setPalette(darkPalette);
+
+        qApp->setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }");
+    }
+#endif
+
     // Deprecated in Qt5, Qt5 geht davon aus, dass der Sourcecode UTF-8 kodiert ist
 #if QT_VERSION < 0x050000
     	QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
@@ -79,6 +112,7 @@ int main(int argc, char *argv[])
 #ifdef HAVE_PYTHON
     Py_InitializeEx(0);
 #endif
+
 
     CWmClient Client;
     if (!Client.Init(argc,argv,&a)) {
