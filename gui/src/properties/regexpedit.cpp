191d60f254d00d6430959e81bd0fab54724a6a91
pfedick <patrick@pfp.de>
2022-04-17 19:08:44 +0200

    updated copyright

--- gui/src/properties/regexpedit.cpp
+++ gui/src/properties/regexpedit.cpp
@@ -1,13 +1,7 @@
 /*
  * This file is part of WinMusik 3 by Patrick Fedick
  *
- * $Author: pafe $
- * $Revision: 1.5 $
- * $Date: 2010/10/10 10:31:01 $
- * $Id: properties.cpp,v 1.5 2010/10/10 10:31:01 pafe Exp $
- *
- *
- * Copyright (c) 2010 Patrick Fedick
+ * Copyright (c) 2022 Patrick Fedick
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
