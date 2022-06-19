f735d1f1954dbf469498d3ae312f542181bf2072
Patrick Fedick <patrick@pfp.de>
2022-06-19 11:03:57 +0200

    updated pplib

--- gui/include/version.h
+++ gui/include/version.h
@@ -1,6 +1,6 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 #define WM_VERSION	"3.7.0"
-#define WM_REVISION	"917"
-#define WM_RELEASEDATE	20220618
+#define WM_REVISION	"921"
+#define WM_RELEASEDATE	20220619
 #endif /* VERSION_H_ */
