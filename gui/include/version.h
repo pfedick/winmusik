151da19295fa3441167900be3a3a94003df64f71
Patrick Fedick <patrick@pfp.de>
2022-08-21 14:18:53 +0200

    fixed Bug un ppl7::File::rename on windows

--- gui/include/version.h
+++ gui/include/version.h
@@ -1,6 +1,6 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 #define WM_VERSION	"3.7.0"
-#define WM_REVISION	"941"
-#define WM_RELEASEDATE	20220810
+#define WM_REVISION	"943"
+#define WM_RELEASEDATE	20220821
 #endif /* VERSION_H_ */
