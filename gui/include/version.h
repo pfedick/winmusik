3d40c2f5a56889b07cbecf570b0441214d1ca925
Patrick Fedick <patrick@pfp.de>
2024-07-22 10:30:09 +0200

    Ignore beatport country codes in artist, when searching

--- gui/include/version.h
+++ gui/include/version.h
@@ -1,6 +1,6 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 #define WM_VERSION "3.7.2"
-#define WM_REVISION "1024"
-#define WM_RELEASEDATE 20240702
+#define WM_REVISION "1030"
+#define WM_RELEASEDATE 20240722
 #endif
