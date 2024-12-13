47ac07a0abe3972d3cce87a901ca6de0c6a9803f
pfedick <fedick@denic.de>
2024-12-13 16:26:00 +0100

    updated pplib, fixed configure on Fedora 41

--- gui/include/version.h
+++ gui/include/version.h
@@ -1,6 +1,6 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 #define WM_VERSION "3.8.0"
-#define WM_REVISION "1040"
-#define WM_RELEASEDATE 20241028
+#define WM_REVISION "1041"
+#define WM_RELEASEDATE 20241213
 #endif
