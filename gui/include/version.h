0046ec851a667959849885a808ae3b1733556bb5
Patrick Fedick <patrick@pfp.de>
2024-12-15 15:01:50 +0100

    added call to qmake in main Makefile

--- gui/include/version.h
+++ gui/include/version.h
@@ -1,6 +1,6 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 #define WM_VERSION "3.8.0"
-#define WM_REVISION "1041"
-#define WM_RELEASEDATE 20241213
+#define WM_REVISION "1043"
+#define WM_RELEASEDATE 20241215
 #endif
